d14e86282649b43d4e8be7b031b0bc8499e8b495
YtaloWill <ytalowillgloria@gmail.com>
2020-05-29 03:34:11 -0300

    Add array exercises, random example

---
+++ arrays/Repeat.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void populateArray(int array[], int length);
+void buscaRepetidos(int v[], int valor, int tam);
+
+int main(){
+    srand(time(NULL));
+    int length = rand() % 30 + 1;
+    int array[length];
+
+    populateArray(array, length);
+    
+    printf("Array Length: %d\n", length);
+    printf("Array: ");
+    
+    int i;
+    for (i = 0; i < length; i++)
+        printf("%d ", array[i]);
+        
+    int test;
+    scanf("\nWrite the value to search: %d", &test);
+    getPosition(array, test, length);
+
+    return 0;
+}
+
+void populateArray(int array[], int length){
+    int i;
+    for (i = 0; i < length; i++)
+        array[i] = rand() % 30 + 1;
+}
+
+void buscaRepetidos(int v[], int valor, int tam){
+    int i, finded = 0;
+    for (i = 0; i < tam; i++){
+        if (v[i] == valor){
+            printf("Posicao: %d\n", i);
+            finded = 1;
+        }
+    }
+    if (!finded)
+        printf("Value not found");
+}
